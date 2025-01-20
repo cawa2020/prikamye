83f9d6d55ac8622ae3422c14ece8b05aafcd479d
laa <2010alapin@gmail.com>
2025-01-20 19:02:04 +0400

    animation

--- src/components/HowWeWork/HowWeWork.tsx
+++ src/components/HowWeWork/HowWeWork.tsx
@@ -1,8 +1,26 @@
+import { useEffect } from 'react'
 import { workPoints } from '../../data/points'
 import Button from '../../shared/ui/Button/Button'
 import './HowWeWork.scss'
 
 const HowWeWork = () => {
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('point--active');
+                }
+            });
+        }, {
+            threshold: 0.2
+        });
+
+        const points = document.querySelectorAll('.point');
+        points.forEach((point) => observer.observe(point));
+
+        return () => observer.disconnect();
+    }, []);
+
     return (
         <section className="how-we-work">
             <div className='left-side'>
